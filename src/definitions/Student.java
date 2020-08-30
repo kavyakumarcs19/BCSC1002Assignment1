21a3968908801d423b214896460ef6f246faf3e9
Kavya Kumar <kavya.kumar_cs19@gla.ac.in>
2020-08-30 23:32:23 +0530

    Add variables for Book.java Add method getISBNNumberOfBook() for Book.java Add method setISBNNumberOfBook() for Book.java Add constructor Book() for Book.java Add string method getNameOfTheAuthor() for Book.java Add string method getNameOfTheBook() for Book.java Add method setNameOfTheBook for Book.java Add method setNameOfTheAuthor for Book.java Add toString() string method for Book.java Add equals() boolean method for Book.java Add hashCode() method for Book.java Add array variable for Library.java Add constructor for Library.java Add constructor for Library.java Add constructor for Library.java Add setMarket() method for Library.java Add comments Add method listOfOurInventory() for Library.java Add toString() string method for Library.java Add equals() boolean method for Library.java Add hashCode() integer method for Library.java Add variables and modules for Student.java Add constructor for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -12,8 +12,19 @@ public class Student {
     Scanner scanner = new Scanner(System.in);
     private String nameOfTheStudent;
     private long rollNumberOfUniversity;
-    private Book[] nameOfTheBooks;
+    private Book[] nameOfTheBook;
     private int codeOfTheIssuedBook = 0;
+
+    public Student(String nameOfTheStudent, long rollNumberOfUniversity, int codeOfTheIssuedBook, Book[] nameOfTheBook) {
+        this.nameOfTheStudent = nameOfTheStudent;
+        this.rollNumberOfUniversity = rollNumberOfUniversity;
+        this.codeOfTheIssuedBook = codeOfTheIssuedBook;
+        this.nameOfTheBook = nameOfTheBook;
+    }
+
+    public Student() {
+        this.nameOfTheBook = new Book[5];
+    }
 
 
 }
