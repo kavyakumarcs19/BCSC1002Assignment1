4e493f25fd37939446858c9b72e3e38773695bb9
Kavya Kumar <kavya.kumar_cs19@gla.ac.in>
2020-08-30 23:29:33 +0530

    Add variables for Book.java Add method getISBNNumberOfBook() for Book.java Add method setISBNNumberOfBook() for Book.java Add constructor Book() for Book.java Add string method getNameOfTheAuthor() for Book.java Add string method getNameOfTheBook() for Book.java Add method setNameOfTheBook for Book.java Add method setNameOfTheAuthor for Book.java Add toString() string method for Book.java Add equals() boolean method for Book.java Add hashCode() method for Book.java Add array variable for Library.java Add constructor for Library.java Add constructor for Library.java Add constructor for Library.java Add setMarket() method for Library.java Add comments Add method listOfOurInventory() for Library.java Add toString() string method for Library.java Add equals() boolean method for Library.java Add hashCode() integer method for Library.java Add variables and modules for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,11 +1,19 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Kavya Kumar
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
  * */
 package definitions;
 
+import java.util.Scanner;
+
 public class Student {
+    Scanner scanner = new Scanner(System.in);
+    private String nameOfTheStudent;
+    private long rollNumberOfUniversity;
+    private Book[] nameOfTheBooks;
+    private int codeOfTheIssuedBook = 0;
+
 
 }
